d02b4d221899a64efdffd4b96242631a76c7e32a
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-09-05 20:16:08 -0300

    first commit

---
+++ src/Perfil.java
@@ -0,0 +1,12 @@
+
+// Classe Perfil
+public class Perfil {
+    // Método principal (main)
+    public static void main(String[] args) {
+        // Variável para armazenar o nome
+        String nome = "Fabrício Baraúna";
+
+        // Imprimir cumprimento personalizado
+        System.out.println("Olá, " + nome + "!");
+    }
+}
