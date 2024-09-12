2dc48f317c9e664dde0493fe69e00ca44bfc29cd
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-09-11 21:19:45 -0300

    casting

--- src/Main.java
+++ src/Main.java
@@ -4,30 +4,34 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int anoDeLancamento = 2022;
+        int anoDeLancamento = 2014;
         boolean incluidoNoPlano = true;
         double notaDoFilme = 4.2;
-
-        System.out.println("**** Esse é o Screen Match **** \n");
-        System.out.println("Filme: Top Gun Marverick");
-        System.out.println("Ano de Lançamento: " + anoDeLancamento);
-        System.out.println("Nota do Filme " + notaDoFilme);
-
-
-        /*Calcular média*/
-        double media = (9.5 + 8.5 + 7.5)/3;
-        System.out.println( "Média: " + media);
+        double media = (9.5 + 8.5 + 7.5)/3; /*Calcular média do filme */
 
         String sinopse;
         sinopse = """
-                    John Wick é um 
-                    lendário assassino de aluguel aposentado, lidando com o luto após perder o 
+                    \n
+                    John Wick é um lendário assassino de aluguel aposentado, lidando com o luto após perder o 
                     grande amor de sua vida. Quando um gângster invade sua casa, 
                     mata seu cachorro e rouba seu carro, ele é forçado 
                     a voltar à ativa e inicia sua vingança.
+                    
                 """;
         System.out.println("Sinopse do Fileme: " + sinopse);
 
+        //casting
+        int classificacao = (int) (media/2);
+
+
+        // Output
+        System.out.println("**** Esse é o Screen Match **** \n");
+        System.out.println("Filme: John Wick");
+        System.out.println("Ano de Lançamento: " + anoDeLancamento);
+        System.out.println("Nota do Filme " + notaDoFilme);
+        System.out.println("A média do filme é: "+ media);
+        System.out.println("Classificação do Filme: " + classificacao);
+        System.out.println("Sinopse do Fileme: " + sinopse);
 
     }
 }
