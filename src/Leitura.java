63a950bcb4cc0be130c5c16520d6d988d849b371
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-09-17 23:18:42 -0300

    leitura de dados

--- src/Leitura.java
+++ src/Leitura.java
@@ -7,13 +7,16 @@ public class Leitura {
         System.out.println("Digite seu filme favorito: ");
         String filme = leitura.nextLine();
 
+        System.out.println("Fale a Sinopse do filme : ");
+        String sinopse = leitura.nextLine();
+
         System.out.println("Digite um ano de Lançamento : ");
         int anoDeLancamento = leitura.nextInt();
 
         System.out.println("Digite a avaliação do fime : ");
         double avaliacao = leitura.nextDouble();
 
-        System.out.println("Seu Filme Favorito é " + filme + " e o ano de laçamento foi  " + anoDeLancamento + " E sua avaliação foi de " + avaliacao);
+        System.out.println("Seu Filme Favorito é " + filme + " e o ano de laçamento foi  " + anoDeLancamento + " E sua avaliação foi de " + avaliacao + " Sinopse: " + sinopse);
 
     }
 }
