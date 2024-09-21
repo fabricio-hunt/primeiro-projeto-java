ee6114e15bf3e489df3ff97216f059bbd6a9c1f4
Fabrício Macedo Baraúna <fabriciobaraunadev@gmail.com>
2024-09-20 21:56:38 -0300

    inicialização de dados

---
+++ src/ContaBancaria.java
@@ -0,0 +1,21 @@
+public class ContaBancaria {
+    private static java.lang.String String;
+/*  Inicialização do cliente
+    Menu de opções
+    Visuaçizações de Dados
+    Enviar Valor
+    Receber Valor
+ */
+    public static void main(String[] args) {
+        String nome = "Fabrício Macedo Baraúna";
+        String  tipodeConta = "Tipo de conta: Corrente";
+        double  saldoDaConta = 4599.95;
+
+        // Variáveis
+        System.out.println("********************\n");
+        System.out.println("Nome: " + nome);
+        System.out.println("Tipo de Conta: " +tipodeConta);
+        System.out.println("Saldo da Conta R$: " + saldoDaConta);
+        System.out.println("********************\n");
+    }
+}
